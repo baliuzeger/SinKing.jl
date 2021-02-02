e92621f5f21acd9b2dca2bb540f2b18bce5fde6e
Bali Sun <baliuzeger@gmail.com>
2021-02-02 17:31:39 +0800

    add pseudo code of task_handler

--- src/SinKing.jl
+++ src/SinKing.jl
@@ -50,12 +50,12 @@ end
 struct LIFNeuron
 end
 
-function evolve(agent::IZNeuron, dt, modifier)
+function evolve(agent::IZNeuron, dt, modifier, task_handler) # should extract the pure IZ model part from act
     
 end
 
 # accept(acceptor) return [] of msgs
-function act(agent::IZNeuron, t, dt, modifier)
+function act(agent::IZNeuron, t, dt, modifier, task_handler)
     if ! isnothing(agent.states.idle_end) && agent.states.idle_end <= t
         delta_exct, delta_inhbt, delta_potential = reduce(
             acc, x -> (acc[1] + x[1], acc[2] + x[2], acc[3] + x[3]),
@@ -73,36 +73,39 @@ function act(agent::IZNeuron, t, dt, modifier)
                                  agent.states.exct_mrkrm_w,
                                  agent.states.inhbt_mrkram_r,
                                  agent.states.inhbt_mrkram_w,
-                                 nothing)
+                                 nothing))
 
         stts = agent.states
         prms = agent.params
-        i_syn = stts.g_ampa(stts.iz_v) +
-            stts.g_nmda(stts.iz_v)((stts.iz_v + 80) / 60)^2 / (1 + ((stts.iz_v + 80) / 60)^2) +
-            stts.g_gaba_a(stts.iz_v + 70) + stts.g_gaba_b(stts.iz_v + 90)
+        i_syn = stts.g_ampa * stts.iz_v +
+            stts.g_nmda * stts.iz_v * ((stts.iz_v + 80) / 60)^2 / (1 + ((stts.iz_v + 80) / 60)^2) +
+            stts.g_gaba_a * (stts.iz_v + 70) + stts.g_gaba_b(stts.iz_v + 90)
         new_v = stts.iz_v + dt * (0.04 stts.iz_v^2 + 5 stts.iz_v + 140 - stts.iz_u - i_syn)
         new_u = stts.iz_u + dt * prms.iz_a (prms.iz_b * stts.iz_v - stts.iz_u)
         if new_v >= 30
             new_v = prms.iz_c
             new_u += prms.iz_d
             rw_exct = stts.exct_mrkrm_r * stts.exct_mrkrm_w
-                 rw_inhbt = stts.inhbt_mrkrm_r * stts.inhbt_mrkrm_w
-                 modifier(agent, IZStates(new_u,
-                                          new_v,
-                                          agent.states.g_ampa,
-                                          agent.states.g_nmdap,
-                                          agent.states.g_gaba_a,
-                                          agent.states.g_gaba_b,
-                                          agent.states.exct_mrkrm_r xxxx,
-                                          agent.states.exct_mrkrm_w xxx,
-                                          agent.states.inhbt_mrkram_r sss,
-                                          agent.states.inhbt_mrkram_w sss,
-                                          t + tau_refraction))
+            rw_inhbt = stts.inhbt_mrkrm_r * stts.inhbt_mrkrm_w
+            modifier(agent, IZStates(new_u,
+                                     new_v,
+                                     agent.states.g_ampa,
+                                     agent.states.g_nmdap,
+                                     agent.states.g_gaba_a,
+                                     agent.states.g_gaba_b,
+                                     agent.states.exct_mrkrm_r, # update
+                                     agent.states.exct_mrkrm_w,
+                                     agent.states.inhbt_mrkram_r,
+                                     agent.states.inhbt_mrkram_w,
+                                     t + tau_refraction))
             for dnr in agent.donors
-                dnr.
-                    end
+                x =1
+            end
+            put_task(tak_handler,) # set as after idle time
+        else
+            put_task(tak_handler, t + dt)
         end
-                         
+    end                 
 end
 
 
