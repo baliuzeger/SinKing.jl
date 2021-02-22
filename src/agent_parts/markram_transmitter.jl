2a09ce87f3928707ca6e8c1d56e28d66690a2391
Bali Sun <baliuzeger@gmail.com>
2021-02-22 15:46:00 +0800

    iz_neuron evolve ok.

--- src/agent_parts/markram_transmitter.jl
+++ src/agent_parts/markram_transmitter.jl
@@ -15,3 +15,22 @@ struct Markramparams{T <: AbstractFloat}
     inhbt_f::T
     inhbt_u::T
 end
+
+struct fire(t, dt)
+    rw_exct = stts.exct_mrkrm_r * stts.exct_mrkrm_w
+    rw_inhbt = stts.inhbt_mrkrm_r * stts.inhbt_mrkrm_w
+    agent.states = IZStates(new_u,
+                            new_v,
+                            agent.states.g_ampa,
+                            agent.states.g_nmdap,
+                            agent.states.g_gaba_a,
+                            agent.states.g_gaba_b,
+                            agent.states.exct_mrkrm_r, # update
+                            agent.states.exct_mrkrm_w,
+                            agent.states.inhbt_mrkram_r,
+                            agent.states.inhbt_mrkram_w,
+                            t + tau_refraction)
+    for dnr in agent.donors
+        x =1
+    end
+end
