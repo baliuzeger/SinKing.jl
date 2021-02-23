d276c9a791a45e99967ee6e06ee04f6e22f1a389
Bali Sun <baliuzeger@gmail.com>
2021-02-23 12:21:25 +0800

    lif_neuron ok.

--- src/agent_parts/iz_neuron.jl
+++ src/agent_parts/iz_neuron.jl
@@ -14,22 +14,22 @@ struct IZParams{T <: AbstractFloat}
     tau_refraction::T
 end
 
-function evolve(t, dt, states::IZStates, params::IZParams, inject_fn, updater, fire_fn, task_fn)
+function evolve(t, dt, states::IZStates, params::IZParams, inject_fn, update, fire_fn, task_fn)
     if isnothing(states.idle_end) || states.idle_end <= t
         i_syn, delta_v = inject_fn()
         new_v = states.v + dt * (0.04 states.v^2 + 5 states.v + 140 - states.u + i_syn) + delta_v
         new_u = states.u + dt * params.iz_a (params.iz_b * states.v - states.u)
         
-        if new_v >= 30
+        if new_v >= 30.0
             new_v = params.iz_c
             new_u += params.iz_d
             fire_fn(t, dt)
             idle_end = t + params.tau_refraction
-            updater(IZStates(new_v, new_u, idle_end))
+            update(IZStates(new_v, new_u, idle_end))
             task_fn(idle_end)
             
         else
-            updater(IZStates(new_v, new_u, nothing))
+            update(IZStates(new_v, new_u, nothing))
             task_fn(t + dt)
         end
     end
--- src/agent_parts/lif_neuron.jl
+++ src/agent_parts/lif_neuron.jl
@@ -7,15 +7,24 @@ end
 
 struct LIFParams{T <: AbstractFloat}
     v_steady::T
+    v_reset::T
     tau_leak::T
     tau_refraction::T
 end
 
-function evolve(t, dt, states::LIFStates, params::LIFParams, inject_fn, updater, fire_fn, task_fn)
+function evolve(t, dt, states::LIFStates, params::LIFParams, inject_fn, update, fire_fn, task_fn)
     if isnothing(states.idle_end) || states.idle_end <= t
         i_syn, delta_v = inject_fn()
-        new_v = dt * (i_syn + params.v_steady - states.v) / params.tau_leak
-    else
+        new_v = dt * (i_syn + params.v_steady - states.v) / params.tau_leak + delta_v
+        if new_v >= 30.0
+            fire_fn(t, dt)
+            idle_end = t + params.tau_refraction
+            update(LIFStates(params.v_reset, idle_end))
+            task_fn(idle_end)
+        else
+            udpate(LIFStates(new_u, nothing))
+            task_fn(t + dt)
+        end
     end
 end
 
