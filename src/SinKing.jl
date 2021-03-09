4bcb7642b1089ba6a29ff81f7fc61c900b99b42e
Bali Sun <baliuzeger@gmail.com>
2021-03-09 14:58:09 +0800

    remove useless notes: signal handler setter.

--- src/SinKing.jl
+++ src/SinKing.jl
@@ -15,16 +15,4 @@ include(".agents/iz_cond_mrkrm.jl")
 include(".agents/lif_simple.jl")
 end
 
-struct LIFNeuron
-end
-
-next step: make synapses. how to deal with passive connections?
-
-the handler of signals should be produced by dependency-injection before each execution. before an execution, the network remember the connections without pre-defined handlers of signals. let vectors of donors and acceptors take setter functions that can perofrm dependency-injection on executions. a setter function set the sognal handlers of both connecting sides.
-
-# function simple_modify_fn(agent: Agent, states)
-#     agent.states = states
-# end
-
-
 end # module
