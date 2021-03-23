ea9ad7cce5ad0cb7661a8542c1d476f7b1751b6d
Bali Sun <baliuzeger@gmail.com>
2021-03-23 15:54:09 +0800

    fix errors of types in dc.jl

--- src/agent_parts/dc.jl
+++ src/agent_parts/dc.jl
@@ -3,13 +3,13 @@ export DCPort, gen_dc_updates
 using ...Network
 using ...Signals
 
-struct DCPort{T <: AbstractFloat, U <: Unsigned}
+struct DCPort{T <: AbstractFloat}
     address::Address
     current::T
-    stack::Vector{TimedDC{T, U}}
+    stack::Vector{TimedDC{T}}
 end
 
-function gen_dc_updates{T <: AbstractFloat, U <: Unsigned}(ports::Vector{DCPort{T, U}})
+function gen_dc_updates(ports::Vector{DCPort{T}}) where{T <: AbstractFloat}
     reduce(ports; init=(zero(T), Vector{DCPort{T, U}}(undef, 0))) do acc, port
         keep, take = take_due_signals(port.stack)
         last_t_dc = reduce((acc2, x2) -> acc2.t < x2.t ? x2 : acc2,
