956def8c4220ee02226b477d851f5af3315c20eb
Bali Sun <baliuzeger@gmail.com>
2021-04-20 12:18:40 +0800

    Delayer's act ok.

--- src/agents/delayer.jl
+++ src/agents/delayer.jl
@@ -1,9 +1,11 @@
 using ...Network
 import ...Network: act, update, state_dict, accept
 
-struct DelayerAgent{T <: AbstractFloat, U <: Signal}
+struct DelayerAgent{T <: AbstractFloat, U::Unsigned, V <: Signal}
     delay::T # delay time
     stack::Vector{(T, U)}
+    donor::Address{U}
+    acceptor::Address{U}
 end
 
 function update(agent::DelayerAgent{T}, updates::Vector{(T, Signal)}) where {T <: AbstractFloat, U <: Signal}
@@ -15,6 +17,30 @@ end
 #     Dict(["stack" => map(state_dict, agent.stack)])
 # end
 
+function act(address::Address{U},
+             agent::DelayerAgent{T, V},
+             dt::T,
+             push_task,
+             update_agent,
+             push_signal) where {T <: AbstractFloat, U <: Unsigned, V <: Signal}
+
+    new_stack, due_stack = reduce(agent.stack; Init=(Vector{(T, V)}(undef, 0), Vector{V}(undef, 0)))
+    do (acc, pair)
+        new_t = pair[1] - dt
+        if new_t >= 0
+            ([acc[1]..., (new_t, pair[2])], acc[2])
+        else
+            (acc[1], [acc[2]..., pair[2]])
+        end
+    end
+
+    foreach(s -> push_signal(agent.acceptor, s), due_stack)
+    update_agent(address, new_stack)
+    if length(new_stack) > 0
+        push_task(address)
+    end
+    
+end
 
 
     
