8aa8528bf3e13bcf08733a91a8b0611f1ccc0d7e
Bali Sun <baliuzeger@gmail.com>
2021-04-20 10:59:46 +0800

    remove time of signals.

--- src/signals.jl
+++ src/signals.jl
@@ -5,67 +5,32 @@ export take_due_signals, name_t_delta_v, connect, Signal,
     TimedDeltaV, TimedExctDeltaCond, TimedInhbtDeltaCond, TimedMarkram, TimedDC, name_t_dc,
     add_acceptor, add_donor, can_add_acceptor, can_add_donor, TimedAdrsDC, name_t_adrs_dc
 
-abstract type TimedSignal <: Signal end
-function after_t end
-
-function take_due_signals(dt::T, signals::Vector{U}) where {T <: AbstractFloat, U <: TimedSignal}
-    keep, take = Vector{U}(undef, 0), Vector{U}(undef, 0)
-    for x in signals
-        if x.t <= dt
-            push!(take, x)
-        else
-            push!(keep, after_t(dt, x))
-        end
-    end
-    return keep, take
-end
-
-struct TimedMarkram{T <: AbstractFloat} <: TimedSignal
-    t::T
+struct MarkramDelta{T <: AbstractFloat} <: Signal
     delta::T
 end
 
-after_t(dt::T, s::TimedMarkram{T}) where {T <: AbstractFloat} = TimedMarkram(s.t - dt, s.delta)
-
-struct TimedExctDeltaCond{T <: AbstractFloat} <: TimedSignal
-    t::T
+struct ExctDeltaCond{T <: AbstractFloat} <: Signal
     delta_cond::T
 end
 
-struct TimedInhbtDeltaCond{T <: AbstractFloat} <: TimedSignal
-    t::T
+struct InhbtDeltaCond{T <: AbstractFloat} <: Signal
     delta_cond::T
 end
 
-struct TimedDeltaCond{T <: AbstractFloat} <: TimedSignal
-    t::T
+struct DeltaCond{T <: AbstractFloat} <: Signal
     delta_cond::T
 end
 
-const name_t_delta_v = "TimedDeltaV"
-struct TimedDeltaV{T <: AbstractFloat} <: TimedSignal
-    t::T
+const name_delta_v = "DeltaV"
+struct DeltaV{T <: AbstractFloat} <: Signal
     delta_v::T
 end
 
-after_t(dt::T, s::TimedDeltaV{T}) where {T <: AbstractFloat} = (s.t - dt, s.delta_v)
-
-const name_t_dc = "TimedDC"
-struct TimedDC{T <: AbstractFloat} <: TimedSignal
-    t::T # start time of the current
-    current::T
+const name_dc_instruction = "DCInstruction"
+struct DCInstruction{T <: AbstractFloat} <: Signal
+    previous::T
+    new::T
 end
-
-after_t(dt::T, s::TimedDC{T}) where {T <: AbstractFloat} = (s.t - dt, s.current)
-
-const name_t_adrs_dc = "TimedAdrsDC"
-struct TimedAdrsDC{T <: AbstractFloat, U <: Unsigned} <: TimedSignal
-    t::T # start time of the current
-    current::T
-    source::Address{U}
-end
-
-after_t(dt::T, s::TimedAdrsDC{T}) where {T <: AbstractFloat} = (s.t - dt, s.current, s.source)
 
 function can_add_acceptor end # (agent, signal_name) -> bool
 function can_add_donor end
