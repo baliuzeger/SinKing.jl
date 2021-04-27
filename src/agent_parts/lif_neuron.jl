5f189392626de6ca1e317dbf1aedad23ec8d7bb3
Bali Sun <baliuzeger@gmail.com>
2021-04-27 12:05:30 +0800

    add v_equilibrium for lif_neuron.

--- src/agent_parts/lif_neuron.jl
+++ src/agent_parts/lif_neuron.jl
@@ -1,10 +1,21 @@
 module LIFNeuron
 export LIFStates, LIFParams, evolve
+using ...Signals
 using Printf
 
-struct LIFStates{T <: AbstractFloat}
+mutable struct LIFStates{T <: AbstractFloat}
     v::T
-    t_refractory::T
+    tau_refractory::T
+    dc::T
+    v_equilibrium::T
+end
+
+function v_eqlbrm(dc::T, tau_refractory::T, v_steady::T) where {T <: AbstractFloat}
+    dc * tau_refractory + v_steady
+end
+
+function LIFStates{T}(v::T, tau_refractory::T, dc::T, v_steady::T) where {T <: AbstractFloat}
+    LIFStates(v, tau_refractory, dc, v_eqlbrm(dc, tau_refractory, v_steady))
 end
 
 struct LIFParams{T <: AbstractFloat}
@@ -15,6 +26,14 @@ struct LIFParams{T <: AbstractFloat}
     lazy_threshold::T
 end
 
+function udpate_dc(states::LIFStates{T},
+                   tau_refractory::T,
+                   v_steady::T,
+                   instruction::DCInstruction{T}) where {T <: AbstractFloat}
+    states.dc = states.dc - instruction.previous + instruction.new
+    states.v_equilibrium = v_eqlbrm(states.dc, tau_refractory, v_steady)
+end
+
 function evolve(dt::T,
                 states::LIFStates,
                 params::LIFParams,
@@ -23,7 +42,7 @@ function evolve(dt::T,
                 fire_fn, # trigger actions of fire.
                 push_task) where {T <: AbstractFloat} # push self's next task by next_t
 
-    if states.t_refractory <= zero(T)
+    if states.tau_refractory <= zero(T)
         i_syn, delta_v = inject_fn()
         new_v = states.v + dt * (i_syn + (params.v_steady - states.v) / params.tau_leak) + delta_v
         if new_v >= 30.0
@@ -37,7 +56,7 @@ function evolve(dt::T,
             end
         end
     else
-        update(LIFStates(states.v, states.t_refractory - dt))
+        update(LIFStates(states.v, states.tau_refractory - dt))
         push_task(dt)
     end
 end
